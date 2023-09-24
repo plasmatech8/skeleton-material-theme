6aef123a15e56c1824d2abb69933be640317ce25
Mark Connelly <com0004@gmail.com>
2023-09-24 16:41:36 +1000

    Add more preview components, move to own file

---
+++ src/lib/PreviewComponents.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { ProgressBar, SlideToggle } from '@skeletonlabs/skeleton';
+	import { ripple } from 'svelte-ripple-action';
+</script>
+
+<!-- Buttons -->
+<div class="grid grid-cols-3 gap-4">
+	<button class="btn variant-filled-primary" use:ripple>primary</button>
+	<button class="btn variant-filled-secondary" use:ripple>secondary</button>
+	<button class="btn variant-filled-tertiary" use:ripple>tertiary</button>
+	<button class="btn variant-filled-success" use:ripple>success</button>
+	<button class="btn variant-filled-warning" use:ripple>warning</button>
+	<button class="btn variant-filled-error" use:ripple>error</button>
+</div>
+<hr class="opacity-50" />
+<!-- Progress Bars -->
+<div class="grid grid-cols-1 gap-4">
+	<ProgressBar meter="bg-primary-500" track="bg-primary-500/20" value={66} max={100} />
+	<ProgressBar meter="bg-secondary-500" track="bg-secondary-500/20" value={50} max={100} />
+	<ProgressBar meter="bg-tertiary-500" track="bg-tertiary-500/20" max={100} />
+</div>
+<hr class="opacity-50" />
+<!-- Badges -->
+<div class="grid grid-cols-4 gap-4 place-items-center">
+	<span class="badge variant-filled-surface">surface</span>
+	<span class="badge variant-filled-primary">primary</span>
+	<span class="badge variant-filled-secondary">secondary</span>
+	<span class="badge variant-filled-tertiary">tertiary</span>
+	<span class="badge badge-glass">glass</span>
+	<span class="badge variant-filled-success">success</span>
+	<span class="badge variant-filled-warning">warning</span>
+	<span class="badge variant-filled-error">error</span>
+</div>
+<hr class="opacity-50" />
+<!-- Slide Toggles -->
+<div class="grid grid-cols-4 gap-4 place-items-center">
+	<SlideToggle name="exampleSliderThree" checked />
+	<SlideToggle name="exampleSliderOne" active="bg-primary-500" checked />
+	<SlideToggle name="exampleSliderTwo" active="bg-secondary-500" checked />
+	<SlideToggle name="exampleSliderFour" active="bg-tertiary-500" checked />
+</div>
--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -1,50 +1,22 @@
 <script lang="ts">
-	import { ProgressBar, SlideToggle } from '@skeletonlabs/skeleton';
-	import { ripple } from 'svelte-ripple-action';
+	import HomeHeroComponents from '$lib/HomeHeroComponents.svelte';
+	import PreviewComponents from '$lib/PreviewComponents.svelte';
 </script>
 
-<div class="container mx-auto p-6">
+<div class="container mx-auto p-6 flex flex-col gap-8">
 	<section class="mb-10">
 		<h1 class="h1 mb-5">Preview</h1>
 		<p>Here is some preview UI that I stole from the docs:</p>
 	</section>
 	<section class="card p-4 space-y-8 col-span-2 lg:col-span-1">
 		<h3 class="h3" data-toc-ignore>Preview</h3>
-		<!-- Buttons -->
-		<div class="grid grid-cols-3 gap-4">
-			<button class="btn variant-filled-primary" use:ripple>primary</button>
-			<button class="btn variant-filled-secondary" use:ripple>secondary</button>
-			<button class="btn variant-filled-tertiary" use:ripple>tertiary</button>
-			<button class="btn variant-filled-success" use:ripple>success</button>
-			<button class="btn variant-filled-warning" use:ripple>warning</button>
-			<button class="btn variant-filled-error" use:ripple>error</button>
-		</div>
-		<hr class="opacity-50" />
-		<!-- Progress Bars -->
-		<div class="grid grid-cols-1 gap-4">
-			<ProgressBar meter="bg-primary-500" track="bg-primary-500/20" value={66} max={100} />
-			<ProgressBar meter="bg-secondary-500" track="bg-secondary-500/20" value={50} max={100} />
-			<ProgressBar meter="bg-tertiary-500" track="bg-tertiary-500/20" max={100} />
-		</div>
-		<hr class="opacity-50" />
-		<!-- Badges -->
-		<div class="grid grid-cols-4 gap-4 place-items-center">
-			<span class="badge variant-filled-surface">surface</span>
-			<span class="badge variant-filled-primary">primary</span>
-			<span class="badge variant-filled-secondary">secondary</span>
-			<span class="badge variant-filled-tertiary">tertiary</span>
-			<span class="badge badge-glass">glass</span>
-			<span class="badge variant-filled-success">success</span>
-			<span class="badge variant-filled-warning">warning</span>
-			<span class="badge variant-filled-error">error</span>
-		</div>
-		<hr class="opacity-50" />
-		<!-- Slide Toggles -->
-		<div class="grid grid-cols-4 gap-4 place-items-center">
-			<SlideToggle name="exampleSliderThree" checked />
-			<SlideToggle name="exampleSliderOne" active="bg-primary-500" checked />
-			<SlideToggle name="exampleSliderTwo" active="bg-secondary-500" checked />
-			<SlideToggle name="exampleSliderFour" active="bg-tertiary-500" checked />
-		</div>
+		<PreviewComponents />
 	</section>
+	<div class="mx-auto">
+		<section
+			class="card bg-gradient-to-tr from-surface-100 dark:from-surface-900 to-primary-100 dark:to-primary-800 p-4 space-y-8 max-w-7xl"
+		>
+			<div class="w-[700px] max-w-7xl mx-auto p-4 py-16 md:py-24"><HomeHeroComponents /></div>
+		</section>
+	</div>
 </div>
