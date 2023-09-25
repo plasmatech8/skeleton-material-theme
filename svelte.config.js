c0cad2fa4bc5dd31561312ffc50e86db3cecf00e
Mark Connelly <40431112+plasmatech8@users.noreply.github.com>
2023-09-25 14:44:34 +1000

    Use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 
@@ -8,7 +8,7 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [ vitePreprocess()],
-	
+
 	vitePlugin: {
 		inspector: true,
 	},
