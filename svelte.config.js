c5bcc10df359e13d8f8a50bedd41e0bb58dc3365
Mark Connelly <com0004@gmail.com>
2023-09-26 21:40:21 +1000

    Fix base path for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,26 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
+	preprocess: [vitePreprocess()],
 
 	vitePlugin: {
-		inspector: true,
+		inspector: true
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
 	}
 };
 export default config;
